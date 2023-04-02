ac4137966be4ac93c0250f17bbf3ed6e341149e8
Taylor MacInytre <ty452018@dal.ca>
2023-04-02 12:55:48 -0300

    added logic to not spawn fruit on tiles around snakes head

--- app/src/main/java/com/example/snakegame/SnakeMap.kt
+++ app/src/main/java/com/example/snakegame/SnakeMap.kt
@@ -21,10 +21,15 @@ class SnakeMap(val rows: Int, val cols: Int) {
     fun generateFood() {
         //TODO: Fruits may be generated at the location of the snake
         val emptyTiles = ArrayList<Pair<Int, Int>>()
+        //return an Arraylist<Pair<int,int>> of coordinates around snake's head
+        val coordinatesNearHead = coordinatesNearSnakeHead()
         for (i in 0 until rows) {
             for (j in 0 until cols) {
-                if(containsSnake(i, j)) {
+                val pair = Pair(i, j)
+                if (containsSnake(i, j)) {
                     map[i][j] = MapBlock.SNAKE //check with Taicheng that this doesn't interfere with his logic
+                } else if(coordinatesNearHead.indexOf(pair) != -1) {
+                    //coordinate is too close to snake's head, don't use it to spawn fruit
                 } else if (map[i][j] == MapBlock.EMPTY) {
                     emptyTiles.add(Pair(i, j))
                 }
@@ -62,4 +67,60 @@ class SnakeMap(val rows: Int, val cols: Int) {
         }
         return false
     }
+
+    /*** This function creates an arrayList of tiles that are around the snakes head, will be used
+     * to ensure fruit doesn't spawn close to snakes head, 3x4 with the snake's head in the short side
+     * DISCLAIMER: it's ugly because the area around the snakes head isn't square, so it has to be
+     * calculated differently for each cardinal direction
+     */
+    fun coordinatesNearSnakeHead(): ArrayList<Pair<Int, Int>>{
+        val coordinatesNearSnakeHead = ArrayList<Pair<Int, Int>>()
+        val headRow = SnakeBody().body[0].row
+        val headCol = SnakeBody().body[0].column
+
+        if(SnakeBody().direction.equals("left")){
+            for(i in (headRow-1) until headRow+2 ){
+                for(j in (headCol-3) until headCol+1){
+                    if(i>=0 && j>=0 && i<rows && j<cols) {
+                        //check value is on game board, if it is, add it to arraylist
+                        val pair = Pair<Int, Int>(i, j)
+                        coordinatesNearSnakeHead.add(pair)
+                    }
+                }
+            }
+        } else if (SnakeBody().direction.equals("right")) {
+            for(i in (headRow-1) until headRow+2){
+                for(j in headCol until headCol+4){
+                    if(i>=0 && j>=0 && i<rows && j<cols) {
+                        //check value is on game board, if it is, add it to arraylist
+                        val pair = Pair<Int, Int>(i, j)
+                        coordinatesNearSnakeHead.add(pair)
+                    }
+                }
+            }
+
+        } else if (SnakeBody().direction.equals("down")) {
+            for(i in (headRow-3) until headRow+1){
+                for(j in (headCol-1) until (headCol+2)){
+                    if(i>=0 && j>=0 && i<rows && j<cols) {
+                        //check value is on game board, if it is, add it to arraylist
+                        val pair = Pair<Int, Int>(i, j)
+                        coordinatesNearSnakeHead.add(pair)
+                    }
+                }
+            }
+        } else /*direction == up*/{
+            for(i in headRow until (headRow+4)){
+                for(j in (headCol-1) until (headCol+2)){
+                    if(i>=0 && j>=0 && i<rows && j<cols) {
+                        //check value is on game board, if it is, add it to arraylist
+                        val pair = Pair<Int, Int>(i, j)
+                        coordinatesNearSnakeHead.add(pair)
+                    }
+                }
+            }
+        }
+        return coordinatesNearSnakeHead
+    }
+    
 }
