18c523b35413a253ccc925ef836c84ebfb87f736
Aditya Sharma <ad437951@dal.ca>
2023-04-08 05:55:06 -0300

    added scoreboard on game screen and session functions to update and retrieve session score

--- app/src/main/java/com/example/snakegame/Score.kt
+++ app/src/main/java/com/example/snakegame/Score.kt
@@ -1,12 +1,18 @@
 package com.example.snakegame
 
+import android.content.Context
+import android.content.SharedPreferences
+
+
 /*** Score Class
  *   Author: Aditya Sharma (B00827775)
  *   This class keeps track of the current score and high score of the game ***/
 
-class Score() {
+class Score(context: Context) {
     var score = 0
     var highScore = 0
+    private val ref: Context = context
+    private lateinit var pref: SharedPreferences
 
     /*** This adds a point to the score and checks if the current score has exceeded the high score.
      *   If it has, it replaces the high score ***/
@@ -16,9 +22,20 @@ class Score() {
             replaceHighScore(score)
         }
     }
-    /*** return the current score value ***/
+    /*** update the high score value ***/
     private fun replaceHighScore(score: Int) {
         highScore = score;
+        saveSession(highScore)
+    }
+    private fun saveSession(score: Int) {
+        val pref = ref.getSharedPreferences("mysharedpref", Context.MODE_PRIVATE)
+        val editor=pref.edit()
+        editor.putInt("highScore",score)
+        editor.putString("name","User")
+        editor.commit()
+    }
+    fun returnSession(): SharedPreferences {
+        return pref;
     }
     /*** return the current score value ***/
     fun getGameScore(): Int {
